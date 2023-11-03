79781cf1610ba39c1706d7a2cfba221383a3cfb4
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-11-03 13:59:59 +0100

    ability for post authors to edit and delete their own posts

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
