83784bfc33dad8edbe151e03501f59a90d2b0d47
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-11-07 13:40:02 +0100

    On a new post, author is setted automatically and user can't change it

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -9,7 +9,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'hidden'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             }
 
