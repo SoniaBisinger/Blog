96941fd7fb6ca3d03037c3e1df4a16a9bb0c29cc
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-12-08 15:30:38 +0100

    Trying to push for Vercel

--- ablog/ablog/build_files.sh
+++ ablog/ablog/build_files.sh
@@ -2,3 +2,4 @@ pip install -r requirements.txt
 popd
 python3.9 manage.py collectstatic
 python3.9 manage.py makemigrations
+python3.9 manage.py migrate
--- ablog/ablog/settings.py
+++ ablog/ablog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
