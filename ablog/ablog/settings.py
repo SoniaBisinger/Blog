3ad7ea6f933e898c2e66cfbc1c3ffc8411540a7b
Sonia Bisinger <sonia.bisinger@gmail.com>
2024-01-04 14:58:05 +0100

    Now superuser can modifiy posts event if is not the author

--- ablog/ablog/settings.py
+++ ablog/ablog/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', '.vercel.app', 'localhost']
 
